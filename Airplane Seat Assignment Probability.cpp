21d88ca3db0eb7fa6b1fed6463f38a5f93a30804
Animesh  Singh <animeshrathore255@gmail.com>
2024-01-21 01:04:41 +0530

    Update Airplane Seat Assignment Probability.cpp

--- Airplane Seat Assignment Probability.cpp
+++ Airplane Seat Assignment Probability.cpp
@@ -1,7 +1,16 @@
 class Solution {
 public:
+double f(int n,vector<double> &dp){
+    if(n==1) return 1.0;
+
+    if(dp[n]!=-1) return dp[n];
+
+    return dp[n] = (1.0/n + ((n-2.0)/n) * f(n-1,dp)); 
+}
+
     double nthPersonGetsNthSeat(int n) {
-       if(n>=2) return (double)1/2;
-       return 1;
+        vector<double> dp(n+1,-1);
+      return f(n,dp);
+       
     }
 };
